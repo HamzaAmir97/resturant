6c26b09355b0b12019980af611fad62243b5c301
Hmazah <alhamza2012@gmail.com>
2024-03-20 10:00:00 +0200

    add  full repo

--- restorant/src/App.tsx
+++ restorant/src/App.tsx
@@ -27,7 +27,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="read-the-docs text-center text-8xl text-sky-500">
         Click on the Vite and React logos to learn more
       </p>
 
