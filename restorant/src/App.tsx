00c99380f99853d40f25332bf312df4e2f82b1ec
Hmazah <alhamza2012@gmail.com>
2024-03-23 10:00:00 +0200

    fix layout

--- restorant/src/App.tsx
+++ restorant/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
 
-      <div className="flex flex-col items-start h-screen justify-center  bg-red-600 ">
+      <div className="flex flex-col items-start h-screen justify-between  bg-red-600 ">
       <div  className=' w-full  bg-blue-500  border-8 border-black'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo  " alt="Vite logo" />
